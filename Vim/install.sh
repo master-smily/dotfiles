1b1fca1c389e82ebf364166df14435fc12a2b01d
William Cedervall <dv17wcl@cs.umu.se>
2019-04-18 18:18:44 +0200

    Improve install.sh and add  zsh-syntax-highlight
    check if repo has already been installed before cloning

--- Vim/install.sh
+++ Vim/install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-
-git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+    git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
--- Zsh/install.sh
+++ Zsh/install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 cd $HOME/.dotfiles
 git submodule update --init --recommend-shallow "Zsh/.oh-my-zsh"
+
+ZSH_PLUG_DIR=${ZSH_CUSTOM:-Zsh/.oh-my-zsh/custom}/plugins
+ZSH_SYNTAX_H=${ZSH_PLUG_DIR}/zsh-syntax-highlighting/ 
+if [ -d $ZSH_SYNTAX_H ]; then
+    cd $ZSH_SYNTAX_H
+    git pull
+else
+    git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_SYNTAX_H
+fi
