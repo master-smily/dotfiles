58a4a8af5309ff029e1dffbcc16fd52d96a8e0fd
William Cedervall <dv17wcl@cs.umu.se>
2020-10-05 17:55:33 +0200

    ruby gem path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 set -gx default_user dv17wcl
 set -gx EDITOR vim
 set -gx fish_prompt_pwd_dir_length 4
+set -gx PATH $PATH $HOME/.gem/ruby/2.5.0/bin
 
 alias tree="tree -L 4 --filelimit=16"
 alias ls="ls --classify --sort=version --time-style=long-iso --color=auto"
